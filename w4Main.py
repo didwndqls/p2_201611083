5dda2c39596d3377641cc00521a77c7e0a030f48
didwndqls <didwndqls@naver.com>
2016-03-29 18:35:42 +0900

    과제
    201611083

---
+++ w4Main.py
@@ -0,0 +1,11 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+def makeSwirSquare(size,bigger,sides,angle):
+    for i in range(sides):
+        if(i%2):
+            size=size+bigger
+            t1.fd(size)
+            t1.right(angle)
+makeSwirSquare(10,10,30,90)
+wn.exitonclick()
