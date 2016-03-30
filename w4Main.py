5ce595b5bd858654a42aa3a37fa6fe8ae4c8bda4
didwndqls <didwndqls@naver.com>
2016-03-30 12:04:54 +0900

    Update w4Main.py

--- w4Main.py
+++ w4Main.py
@@ -9,3 +9,19 @@ def makeSwirSquare(size,bigger,sides,angle):
             t1.right(angle)
 makeSwirSquare(10,10,30,90)
 wn.exitonclick()
+
+#바람개비 그리기 다이어그램  
+ %%plantuml  
+ @startuml 
+  
+start  
+ :set size, bigger, sides, angle;  
+ repeat  
+ if (i is multiples of 2) then  
+     :size=existing size+bigger;  
+ endif  
+ :t1.fd(size);  
+ :t1.right(angle);  
+ repeat while(sides)  
+ stop  
+ @enduml  
