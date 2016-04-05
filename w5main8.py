e0c6267181d48b4f2879f58a7c3577b73ec1df4a
didwndqls <didwndqls@naver.com>
2016-04-05 11:48:32 +0900

    BMI Measuring instrument

---
+++ w5main8.py
@@ -0,0 +1,14 @@
+﻿Weight=input("please enter your weight(kg):")
+Height=input("please enter your height(m):")
+BMI=Weight/(Height*Height)
+print BMI
+if (BMI<=18.5):
+    print "low weight"
+elif (18.5<BMI<25.0):
+    print "normal weight"
+elif (25.0<=BMI<30.0):
+    print "over weight"
+elif (30.0<=BMI):
+    print "obesity"
+else:
+    print "Error"
