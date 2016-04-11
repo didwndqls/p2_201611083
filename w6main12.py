c6d75fa6ba013a9aa02fc2f9b346216b455804cb
didwndqls <didwndqls@naver.com>
2016-04-11 19:47:35 +0900

    1~1000사이에 4로 나눠지고 5로 나눠지지 않는 수들의 합

---
+++ w6main12.py
@@ -0,0 +1,32 @@
+﻿def List():
+    x=list()
+    for i in range(0,1001):
+        if (i%4==0 and i%5!=0):
+            x.append(i)
+    print x
+    print "---------------------------------------------------------------------"
+    
+
+def sumList(aList):
+    x=list()
+    for i in range(0,aList):
+        if (i%4==0 and i%5!=0):
+            x.append(i)
+    mysum=0
+    for j in range(0,len(x)):
+        mysum=mysum+x[j]
+    return mysum
+
+def lab6():
+    List()
+    aList=1001
+    labsum=sumList(aList)
+    print labsum
+def main():
+    lab6()
+
+
+if __name__=="__main__":
+        main()
+        
+wn=raw_input()
