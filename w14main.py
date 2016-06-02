a9a5ad365b8293b7943300c16ec0dfaab0156c6b
didwndqls <didwndqls@naver.com>
2016-06-02 16:20:41 +0900

    강아지 소리

---
+++ w14main.py
@@ -0,0 +1,10 @@
+﻿Dogname=raw_input("Dog name :")
+Dogtalk=raw_input("Dog talk :")
+class Dog(object):
+    def __init__(self, name, sound):
+        self.name=name
+        self.sound=sound
+    def talkDog(self):
+        print self.name, "talk", self.sound
+question=Dog(Dogname, Dogtalk)
+question.talkDog()
